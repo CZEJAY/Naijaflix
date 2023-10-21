ec84ef63f1a894b8b1f2a620328694770b5b8c8b
CZEJAY <calebjimmy67@gmail.com>
2023-10-21 08:41:38 -0700

    router

--- components/ViewMore.tsx
+++ components/ViewMore.tsx
@@ -6,7 +6,7 @@ const ViewMore = () => {
     const router = useRouter()
     return (
         <div className='flex justify-center mt-10 mb-10'>
-            <button onClick={() => router.push(`/movies`)} className='text-white font-semibold w-1/2 cursor-pointer px-5 py-2 rounded-lg bg-[#01b4e4] hover:bg-black/10 transition ease-in-out duration-300'>View More</button>
+            <button onClick={() => router.push(`#`)} className='text-white font-semibold w-1/2 cursor-pointer px-5 py-2 rounded-lg bg-[#01b4e4] hover:bg-black/10 transition ease-in-out duration-300'>View More</button>
         </div>
     )
 }
