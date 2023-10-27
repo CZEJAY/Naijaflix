11c16ac4eafa41bd04f522be03f90d5a2684387a
CZEJAY <calebjimmy67@gmail.com>
2023-10-27 06:35:04 -0700

    search layout

--- app/(root)/search/[query]/page.tsx
+++ app/(root)/search/[query]/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import MovieComponent from '@/components/MovieComponent'
 import ViewMore from '@/components/ViewMore'
+import { Skeleton } from '@/components/ui/skeleton'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
@@ -13,6 +14,7 @@ type props = {
 
 const page = ({ params }: props) => {
   const [search, setSearch] = useState<any>([])
+  const [page, setPage] = useState<number>()
   useEffect(() => {
     const getMovie = async () => {
       await axios.post('/api/search', {
@@ -20,7 +22,8 @@ const page = ({ params }: props) => {
       })
         .then((res) => {
           setSearch(res.data.results)
-          console.log(res.data);
+          setPage(res.data.total_pages)
+          console.log(res.data.total_pages);
         })
       // console.log(search);
     }
@@ -28,11 +31,11 @@ const page = ({ params }: props) => {
   }, [])
 
   const handlePagination = () => {
-    // console.log("pagination");
+    page && page > 1 && setPage(page - 1)
     const getMovie = async () => {
       await axios.post('/api/search', {
         query: params.query,
-        page: 2
+        page
       })
         .then((res) => {
           setSearch(res.data.results)
@@ -41,28 +44,48 @@ const page = ({ params }: props) => {
       // console.log(search);
     }
     getMovie()
-  
+
   }
 
   return (
     <>
       <div
-        className='p-1 px-2 flex justify-center w-full h-full items-start gap-4 flex-wrap'
+        className='p-1 px-2 flex justify-center  items-start gap-4 flex-wrap'
       >
-        <div className="flex self-start w-full">
-          <h1 className='text-gray-950  dark:text-white'>Search result for: <q>{params.query}</q></h1>
-          <p className='text-gray-950 dark:text-white ml-auto'>{search.length} results</p>
-        </div>
+        {
+          search && search.length > 0 ? (
+            <div className="flex self-start border bg-gray-950 rounded-md py-1 px-2 items-center justify-between w-full">
+              <h1 className='text-gray-950  dark:text-white'>Search result for: <q>{params.query}</q></h1>
+              <p className='text-gray-950 self-center  dark:text-white'>Page 1 of {page} pages</p>
+              <p className='text-gray-950 dark:text-white '>{search.length} results</p>
+            </div>
+          ) : (
+            <Skeleton className="flex self-start border rounded-md py-4 px-2 items-center justify-between w-full" />
+          )
+        }
         {
           search && search.length > 0 ? search.map((movie: any) => (
-            <div className="flex flex-row self-center flex-wrap">
+            <div className="flex flex-row  self-center flex-wrap">
               <MovieComponent key={movie.id} item={movie} />
             </div>
           ))
-            : <h1>No Result Found</h1>
+            : (
+              <div className="flex gap-4 mt-2 self-center flex-wrap">
+                <Skeleton className="w-[200px] h-[250px] rounded-lg" />
+                <Skeleton className="w-[200px] h-[250px] rounded-lg" />
+                <Skeleton className="w-[200px] h-[250px] rounded-lg" />
+                <Skeleton className="w-[200px] h-[250px] rounded-lg" />
+                <Skeleton className="w-[200px] h-[250px] rounded-lg" />
+                <Skeleton className="w-[200px] h-[250px] rounded-lg" />
+                <Skeleton className="w-[200px] h-[250px] rounded-lg" />
+                <Skeleton className="w-[200px] h-[250px] rounded-lg" />
+                <Skeleton className="w-[200px] h-[250px] rounded-lg" />
+                <Skeleton className="w-[200px] h-[250px] rounded-lg" />
+              </div>
+            )
         }
       </div>
-      {search && search.length > 0 && <div onClick={handlePagination} className=""><ViewMore /></div> }
+      {search && search.length > 0 && <div onClick={handlePagination} className=""><ViewMore /></div>}
     </>
   )
 }
